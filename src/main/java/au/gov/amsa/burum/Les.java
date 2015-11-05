18fb6282dbc7f977f7f44ddf97fdcd3143e209d0
Dave Moten <davidmoten@gmail.com>
2015-11-05 12:29:14 +1100

    dev

--- src/main/java/au/gov/amsa/burum/Les.java
+++ src/main/java/au/gov/amsa/burum/Les.java
@@ -2,14 +2,14 @@ package au.gov.amsa.burum;
 
 import java.io.IOException;
 
-public final class LES {
+public final class Les {
 
     private final String host;
     private final int port;
     private final String username;
     private final String password;
 
-    public LES(String host, int port, String username, String password) {
+    public Les(String host, int port, String username, String password) {
         this.host = host;
         this.port = port;
         this.username = username;
